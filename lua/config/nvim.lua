35b2f69e30afca3aecdfdb0fedd29549534ef1f8
Rafael Marques Campos <markesxd@gmail.com>
2025-08-03 17:47:57 -0300

    Adding configs
    Added keymap to show LSP diagnostics
    Setting spelllang to Portuguese
    Showing some blank characters

--- lua/config/nvim.lua
+++ lua/config/nvim.lua
@@ -7,3 +7,6 @@ vim.opt.ignorecase = true
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 vim.opt.updatetime = 100
+vim.opt.spelllang = 'pt'
+vim.opt.list = true
+vim.o.listchars = 'space:.,tab:>-'
